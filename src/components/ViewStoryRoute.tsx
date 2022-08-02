7c90ef6af2b2321d4d8499341644b8b76c547460
Milo <miloprince92@gmail.com>
2022-08-02 09:03:22 -0400

    formatting

--- src/components/ViewStoryRoute.tsx
+++ src/components/ViewStoryRoute.tsx
@@ -14,14 +14,13 @@ const ViewStoryRoute = () => {
         <>
           <h2>Mad Lib by: {story.author}</h2>
           <p>
-            As I tried to escape {story.word1}, I was apprehended by
+            As I tried to escape {story.word1}, I was apprehended by{" "}
             {story.word2}. They were absolutely livid at my departure, called me
-            a {story.word3}
-            piece of toast, and told me I would never work in the {story.word4}
-            business again. I was going to have to wear an "I'm with Stupid"
-            novelty T-shirt for
-            {story.word5} years. One of these days I'll escape my {story.word3}-
-            prison, and I'll be able to tell everyone {story.word6}.
+            a {story.word3} piece of toast, and told me I would never work in
+            the {story.word4} business again. I was going to have to wear an
+            "I'm with Stupid" novelty T-shirt for {story.word5} years. One of
+            these days I'll escape my {story.word3} prison, and I'll be able to
+            tell everyone {story.word6}.
           </p>
         </>
       )}
